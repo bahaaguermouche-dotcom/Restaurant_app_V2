153851b11030723f211d4169fa78203330a6d2b8
bozar mohamed <bozar.mohamed@example.com>
2025-12-19 00:55:38 +0100

    Version finale2

--- restaurant-react/backend/server.js
+++ restaurant-react/backend/server.js
@@ -22,10 +22,12 @@ import { Server } from 'socket.io';
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1);
+
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: "http://localhost:5173", // URL frontend Vite
+        origin: process.env.FRONTEND_URL || "http://localhost:5173",
         methods: ["GET", "POST"]
     }
 });
@@ -33,7 +35,10 @@ const io = new Server(httpServer, {
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
